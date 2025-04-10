831adc495b46d5a2833243fb6b6a53b8e8f8bb63
Niklas Carlsén <niklascarlsen@live.se>
2025-04-10 08:23:42 +0200

    Refactor Homepage component to update image loading properties by removing explicit width and height attributes, switching to 'fill' for responsive design. Change image sizes from '100vw' to '90vw' for better layout optimization.

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -18,11 +18,12 @@ export default function Homepage() {
         <Image
           src={primaryImage}
           alt='Landing-Page-Dam'
-          width={1920}
-          height={1080}
+          // width={1920}
+          // height={1080}
+          fill
           priority={true}
           loading='eager'
-          sizes='100vw'
+          sizes='90vw'
           quality={100}
           className={`hidden md:block object-cover w-full h-full absolute top-0 left-0 transition-opacity duration-700 ${
             currentView === 'dam' ? 'opacity-100' : 'opacity-0'
@@ -31,10 +32,12 @@ export default function Homepage() {
         <Image
           src={secondaryImage}
           alt='Landing-Page-Herr'
-          width={1920}
-          height={1080}
-          loading='lazy'
-          sizes='100vw'
+          // width={1920}
+          // height={1080}
+          fill
+          priority={true}
+          loading='eager'
+          sizes='90vw'
           className={`hidden md:block object-cover w-full h-full absolute top-0 left-0 transition-opacity duration-700 ${
             currentView === 'herr' ? 'opacity-100' : 'opacity-0'
           }`}
@@ -42,11 +45,12 @@ export default function Homepage() {
         <Image
           src={primaryMobileImage}
           alt='Landing-Page-Dam-Mobil'
-          width={820}
-          height={880}
+          // width={820}
+          // height={880}
+          fill
           priority={true}
           loading='eager'
-          sizes='100vw'
+          sizes='90vw'
           quality={100}
           className={`block md:hidden object-cover w-full h-full absolute top-0 left-0 transition-opacity duration-700 ${
             currentView === 'dam' ? 'opacity-100' : 'opacity-0'
@@ -55,10 +59,11 @@ export default function Homepage() {
         <Image
           src={secondaryMobileImage}
           alt='Landing-Page-Herr-Mobil'
-          width={820}
-          height={880}
+          // width={820}
+          // height={880}
+          fill
           loading='lazy'
-          sizes='100vw'
+          sizes='90vw'
           className={`block md:hidden object-cover w-full h-full absolute top-0 left-0 transition-opacity duration-700 ${
             currentView === 'herr' ? 'opacity-100' : 'opacity-0'
           }`}
